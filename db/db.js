accad3791890f1c2f4d2fdd315393429e2695431
Manager <manager.companyy92@gmail.com>
2025-12-08 20:04:54 +0500

    Create db.js

---
+++ db/db.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+import pkg from 'pg';
+dotenv.config();
+
+const { Pool } = pkg;
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
